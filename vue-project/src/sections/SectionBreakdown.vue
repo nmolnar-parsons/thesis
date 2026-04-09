97f4ece707ac00e56542811d8628c185b52ac755
nmolnar-parsons <molnn656@newschool.edu>
2026-04-09 11:51:17 -0400

    changing a bit of the copy

--- vue-project/src/sections/SectionBreakdown.vue
+++ vue-project/src/sections/SectionBreakdown.vue
@@ -10,8 +10,19 @@ import CopyBlock from '../components/layout/CopyBlock.vue'
   <StorySection id="breakdown" height="min-screen" width="full" background="#e2e8f0">
     <SectionGrid :columns="12" gap="1.25rem" align="center">
       <div class="story-copy">
-        <CopyBlock title="A tuna is a tuna">
-          <p>A tuna is a tuna</p>
+        <CopyBlock title="Limited Resources">
+          <p>
+            Sushi is made of two (optionally three) ingredients. Of these ingredients, fish is the limiting factor.
+          </p>
+          <p>
+            Tuna is the the current king of sushi fish. It grew in popularity in post-war Japan, and became the poster child for the global sushi boom.
+            Tuna remains the most popular cut of sushi amonst Japanese sushigoers. This is despite how expensive it can be. Demand for tuna
+            has turned the fish into the beef of the sea, able to be caught and sold at much higher margins than most marine life.
+          </p>
+          <p>
+            Demand for tuna driven by sushi focuses on a few species in ascending desirability and price: Bigeye, Yellowfin, and the three Bluefin species
+            (Pacific, Southern, and Atlantic).
+          </p>
         </CopyBlock>
       </div>
     </SectionGrid>
